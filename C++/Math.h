065efc74daba54e873ac7291f7d6f3eca1342ed2
Tobias Rohner <rohner.tobias@gmail.com>
2017-04-30 16:28:44 +0200

    Added function to check for powers of two

--- C++/Math.h
+++ C++/Math.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include <limits>
+#include <cassert>
+
 
 //A compile-time abs function
 template<typename T>
 constexpr T cabs(T n)
 {
 	return n < 0 ? -n : n;
+}
+
+
+//Check for power of two
+template<typename T>
+constexpr bool isPower2(T n)
+{
+	assert(std::numeric_limits<T>::is_integer);
+	return !((n - 1) & n);
 }
