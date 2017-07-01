abfbde04ddcd06deee7dd3fe918a7b12190fabab
Tobias Rohner <rohner.tobias@gmail.com>
2017-07-01 16:26:51 +0200

    Added linear interpolation

--- C++/Math.h
+++ C++/Math.h
@@ -16,6 +16,21 @@ inline constexpr T cabs(T n)
 template<typename T>
 inline constexpr bool isPower2(T n)
 {
-	assert(std::numeric_limits<T>::is_integer);
+	static_assert(std::numeric_limits<T>::is_integer);
 	return !((n - 1) & n);
+}
+
+
+//Only return the trailing digits of d
+inline constexpr double trailing(double d)
+{
+	return d - (uint64_t)d;
+}
+
+
+//Linear Interpolation between d1 and d2 with the factor fac
+template<typename T>
+inline constexpr T lerp(T d1, T d2, double fac)
+{
+	return fac * d2 + (1.0 - fac) * d1;
 }
